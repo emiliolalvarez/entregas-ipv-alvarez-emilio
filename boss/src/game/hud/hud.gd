1f7385d8273d9cee77424cbecedb429e20141896
Emilio Alvarez <emiliolalvarez@gmail.com>
2023-10-24 13:16:57 -0300

    Remove unused state

--- boss/src/game/hud/hud.gd
+++ boss/src/game/hud/hud.gd
@@ -2,9 +2,6 @@ extends Control
 
 onready var texture_progress = $Control/TextureProgress
 
-
-
 func _on_hp_changed(current_hp, max_hp):
-	print("hp changed to " + String(current_hp))
 	texture_progress.max_value = max_hp
 	texture_progress.value = current_hp
