7c32a14852e364297e9aa21f9a4fee505d201adb
Emilio Alvarez <emiliolalvarez@gmail.com>
2023-11-02 22:44:59 -0300

    added percentage visor

--- boss/src/game/hud/hud.gd
+++ boss/src/game/hud/hud.gd
@@ -1,7 +1,9 @@
 extends Control
 
 onready var health_progress = $Control/HelthProgress
+onready var health_percentage = $Control/Circle/HealthPercentage
 
 func _on_hp_changed(current_hp, max_hp):
 	health_progress.max_value = max_hp
 	health_progress.value = current_hp
+	health_percentage.text = String(round(health_progress.value * 100 / max_hp)) + '%'
