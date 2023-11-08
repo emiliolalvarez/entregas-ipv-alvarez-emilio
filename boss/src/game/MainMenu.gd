6c4b1c3cda378970fd77bfe0dbb5cbd1ef117b9b
Emilio Alvarez <emiliolalvarez@gmail.com>
2023-11-08 00:10:54 -0300

    Added commands menu

--- boss/src/game/MainMenu.gd
+++ boss/src/game/MainMenu.gd
@@ -8,10 +8,19 @@ export (Texture) var mouse_cursor: Texture
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(mouse_cursor)
 
+func _process(delta):
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().paused = false
+		$Main.show()
 
 func _on_restart_button_pressed() -> void:
 	get_tree().change_scene_to(level_manager_scene)
 
 
 func _on_ExitButton_pressed() -> void:
-	get_tree().quit()
+	hide()
+
+
+func _on_show_commands_button_pressed():
+	hide()
+	$Commands.show()
