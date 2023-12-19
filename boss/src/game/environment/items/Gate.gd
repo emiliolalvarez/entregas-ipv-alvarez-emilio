daaee4c1ac273f798cba3981a3d8df25c1f70ea0
Emilio Alvarez <emiliolalvarez@gmail.com>
2023-12-18 23:27:42 -0300

    move to items folder

---
+++ boss/src/game/environment/items/Gate.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+onready var animation_player = $AnimationPlayer
+
+func _ready():
+	animation_player.play("idle")
+
+func _on_body_entered(body):
+	if body.has_method("notify_hit"):
+		body.notify_hit(body.life)
